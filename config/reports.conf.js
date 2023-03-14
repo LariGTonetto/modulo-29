da038c7baea8211470efdad9f767733a5071ee60
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2023-03-14 16:11:51 -0300

    conf

--- config/reports.conf.js
+++ config/reports.conf.js
@@ -6,8 +6,8 @@ let reportsConf = process.env.REPORT == 'true' ? {
     reporters: ['spec',
         ['allure', {
             outputDir: 'allure-results',
-            disableWebdriverStepsReporting: false,
-            disableWebdriverScreenshotsReporting: false,
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: true,
         }],
         [video, {
             saveAllVideos: true,       // If true, also saves videos for successful test cases
